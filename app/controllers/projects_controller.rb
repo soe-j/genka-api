a11d161c40737ce0579b04338984c1f1e384abb8
soe-j <soeda@val.co.jp>
2016-06-25 13:11:18 +0900

    delete project method to disuse

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :destroy]
 
   # GET /projects
   # GET /projects.json
@@ -10,15 +10,6 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
-  end
-
-  # GET /projects/new
-  def new
-    @project = Project.new
-  end
-
-  # GET /projects/1/edit
-  def edit
   end
 
   # POST /projects
@@ -32,20 +23,6 @@ class ProjectsController < ApplicationController
         format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
-  def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
