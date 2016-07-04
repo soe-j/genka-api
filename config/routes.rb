e1f4007ebf318744009fd10e7de2524addec6e0f
soe-j <soeda@val.co.jp>
2016-07-04 20:58:03 +0900

    routes default format json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :projects
-  root 'top#index'
+  resources :projects, format: "json"
+  root 'top#index', format: "json"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
