18d0ea34a01242df2a8e98aff62464f17e305b46
soe-j <soeda@val.co.jp>
2016-07-07 09:29:43 +0900

    Members Controller like projects one

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,63 +2,31 @@ class MembersController < ApplicationController
   before_action :set_member, only: [:show, :edit, :update, :destroy]
 
   # GET /members
-  # GET /members.json
   def index
     @members = Member.all
+    render json: @members
   end
 
   # GET /members/1
-  # GET /members/1.json
   def show
-  end
-
-  # GET /members/new
-  def new
-    @member = Member.new
-  end
-
-  # GET /members/1/edit
-  def edit
+    render json: @member
   end
 
   # POST /members
-  # POST /members.json
   def create
     @member = Member.new(member_params)
 
-    respond_to do |format|
-      if @member.save
-        format.html { redirect_to @member, notice: 'Member was successfully created.' }
-        format.json { render :show, status: :created, location: @member }
-      else
-        format.html { render :new }
-        format.json { render json: @member.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /members/1
-  # PATCH/PUT /members/1.json
-  def update
-    respond_to do |format|
-      if @member.update(member_params)
-        format.html { redirect_to @member, notice: 'Member was successfully updated.' }
-        format.json { render :show, status: :ok, location: @member }
-      else
-        format.html { render :edit }
-        format.json { render json: @member.errors, status: :unprocessable_entity }
-      end
+    if @member.save
+      render json: @member, status: :created
+    else
+      render json: @member.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /members/1
-  # DELETE /members/1.json
   def destroy
     @member.destroy
-    respond_to do |format|
-      format.html { redirect_to members_url, notice: 'Member was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
