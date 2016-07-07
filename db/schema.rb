11ca9864c7bef321626f861d9002fe05d5063d6f
soe-j <soeda@val.co.jp>
2016-07-07 09:08:32 +0900

    db schema that created stage table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625034239) do
+ActiveRecord::Schema.define(version: 20160705003422) do
 
   create_table "projects", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "stages", force: :cascade do |t|
     t.string "name"
   end
 
