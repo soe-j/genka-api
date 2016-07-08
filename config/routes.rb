9c9eea1ac035081dfdbed1e15c759f3c978577fb
soe-j <soeda@val.co.jp>
2016-07-08 21:07:15 +0900

    all route are formatted with json

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :periods
-  resources :members
+  resources :periods,  format: "json"
+  resources :members,  format: "json"
   resources :stages,   format: "json"
   resources :projects, format: "json"
-  root 'top#index', format: "json"
+  root 'top#index',    format: "json"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
