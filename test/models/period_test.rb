c7f182a735da271ab428f7fc8539169f58d647ff
soe-j <soeda@val.co.jp>
2016-07-10 22:49:05 +0900

    add period model test

--- test/models/period_test.rb
+++ test/models/period_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class PeriodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @project = projects(:one)
+    @stage   = stages(:one)
+    @member  = members(:one)
+    @period = Period.new(
+      project: @project,
+      stage:   @stage,
+      member:  @member
+    )
+  end
+
+  test "should not save period without project" do
+    assert_not Period.new(stage: @stage, member: @member).save
+  end
+
+  test "should not save period without stage" do
+    assert_not Period.new(project: @project, member: @member).save
+  end
+
+  test "should not save period without member" do
+    assert_not Period.new(project: @project, stage: @stage).save
+  end
+
+  test "should save period with auto input start_at" do
+    @period.save
+    assert @period.start_at
+  end
 end
