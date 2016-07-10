e0f95ce9c0fc912e9823adbe94552831df7c0dd9
soe-j <soeda@val.co.jp>
2016-07-10 23:50:39 +0900

    fix test with period's start_at

--- test/models/period_test.rb
+++ test/models/period_test.rb
@@ -8,24 +8,29 @@ class PeriodTest < ActiveSupport::TestCase
     @period = Period.new(
       project: @project,
       stage:   @stage,
-      member:  @member
+      member:  @member,
+      start_at: DateTime.now
     )
   end
 
+  test "should save period" do
+    assert @period.save
+  end
+
   test "should not save period without project" do
-    assert_not Period.new(stage: @stage, member: @member).save
+    assert_not Period.new(stage: @stage, member: @member, start_at: DateTime.now).save
   end
 
   test "should not save period without stage" do
-    assert_not Period.new(project: @project, member: @member).save
+    assert_not Period.new(project: @project, member: @member, start_at: DateTime.now).save
   end
 
   test "should not save period without member" do
-    assert_not Period.new(project: @project, stage: @stage).save
+    assert_not Period.new(project: @project, stage: @stage, start_at: DateTime.now).save
   end
 
   test "should save period with auto input start_at" do
-    @period.save
+    assert Period.new(project: @project, stage: @stage, member: @member).save
     assert @period.start_at
   end
 end
