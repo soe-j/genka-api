b123df7826d8259586aaa3904dda450bf50040d9
soe-j <soeda@val.co.jp>
2016-07-10 22:59:38 +0900

    period columns have validates to not null

--- app/models/period.rb
+++ app/models/period.rb
@@ -2,4 +2,9 @@ class Period < ActiveRecord::Base
   belongs_to :project
   belongs_to :stage
   belongs_to :member
+
+  validates :project,  presence: true, on: :create
+  validates :stage,    presence: true, on: :create
+  validates :member,   presence: true, on: :create
+  validates :start_at, presence: true, on: :create
 end
