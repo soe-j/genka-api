a0f1bb77a454ca7ea0d8fd67bef98ce57a8ff8f6
soe-j <soeda@val.co.jp>
2016-07-10 11:57:04 +0900

    member model and test is like project

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,7 @@
 class Member < ActiveRecord::Base
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    format: /\A[0-9A-Za-z]+\z/,
+    on: :create
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @member = members(:one)
+  end
+
+  test "should save member" do
+    assert @member.save
+  end
+
+  test "should not save member without name" do
+    noname_member = Member.new
+    assert_not noname_member.save
+  end
+
+  test "should not save same name member" do
+    @member.save
+    same_name_member = Member.new(name: @member.name)
+    assert_not same_name_member.save
+  end
+
+  test "should save with name with only Alphanumeric" do
+    alphanumeric_name_member = Member.new(name: "engLISH123")
+    assert alphanumeric_name_member.save
+
+    %w(日本語 abc@def _dec aka:d <ab>).each do |bad_name|
+      assert_not Member.new(name: bad_name).save
+    end
+  end
 end
