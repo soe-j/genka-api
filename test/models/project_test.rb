18c33cd24e1dfe7710d2b759d7e0aec77c480c76
soe-j <soeda@val.co.jp>
2016-07-10 10:59:58 +0900

    add test of project model

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @project = projects(:one)
+  end
+
+  test "should save project" do
+    assert @project.save
+  end
+
+  test "should not save project without name" do
+    noname_project = Project.new
+    assert_not noname_project.save
+  end
+
+  test "should not save same name project" do
+    @project.save
+    same_name_project = Project.new(name: @project.name)
+    assert_not same_name_project.save
+  end
+
+  test "should save with name with only Alphanumeric" do
+    alphanumeric_name_project = Project.new(name: "eng123")
+    assert alphanumeric_name_project.save
+    non_alphanumeric_name_project = Project.new(name: "日本語")
+    assert_not non_alphanumeric_name_project.save
+  end
 end
