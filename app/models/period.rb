4821b11ef8805d449e42264c42f3f4b1854bed51
soe-j <soeda@val.co.jp>
2016-07-11 09:08:24 +0900

    period is set default start_at

--- app/models/period.rb
+++ app/models/period.rb
@@ -3,6 +3,11 @@ class Period < ActiveRecord::Base
   belongs_to :stage
   belongs_to :member
 
+  after_initialize :set_default, if: :new_record?
+  def set_default
+    self.start_at ||= DateTime.now
+  end
+
   validates :project,  presence: true, on: :create
   validates :stage,    presence: true, on: :create
   validates :member,   presence: true, on: :create
