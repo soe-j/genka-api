1a820ae67ac1109b25c72966dc9d45746971ea15
soe-j <soeda@val.co.jp>
2016-07-23 15:48:05 +0900

    add model test

--- test/models/period_test.rb
+++ test/models/period_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class PeriodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @project = projects(:one)
+    @stage   = stages(:one)
+    @member  = members(:one)
+    @period = Period.new(
+      project: @project,
+      stage:   @stage,
+      member:  @member,
+      start_at: DateTime.now
+    )
+  end
+
+  test "should save period" do
+    assert @period.save
+  end
+
+  test "should not save period without project" do
+    assert_not Period.new(stage: @stage, member: @member, start_at: DateTime.now).save
+  end
+
+  test "should not save period without stage" do
+    assert_not Period.new(project: @project, member: @member, start_at: DateTime.now).save
+  end
+
+  test "should not save period without member" do
+    assert_not Period.new(project: @project, stage: @stage, start_at: DateTime.now).save
+  end
+
+  test "should save period with auto input start_at" do
+    assert Period.new(project: @project, stage: @stage, member: @member).save
+    assert @period.start_at
+  end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @project = projects(:one)
+  end
+
+  test "should save project" do
+    assert @project.save
+  end
+
+  test "should not save project without name" do
+    noname_project = Project.new
+    assert_not noname_project.save
+  end
+
+  test "should not save same name project" do
+    @project.save
+    same_name_project = Project.new(name: @project.name)
+    assert_not same_name_project.save
+  end
+
+  test "should save with name with only Alphanumeric" do
+    alphanumeric_name_project = Project.new(name: "engLISH123")
+    assert alphanumeric_name_project.save
+
+    %w(日本語 abc@def _dec aka:d <ab>).each do |bad_name|
+      assert_not Project.new(name: bad_name).save
+    end
+  end
 end
