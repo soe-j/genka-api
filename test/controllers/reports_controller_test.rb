d827216e034cfec3b81c764f92305449780363f1
soe-j <soeda@val.co.jp>
2016-08-08 22:32:47 +0900

    @periods should be only that member's

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @project = projects(:one)
-    @stage   = stages(:one)
-    @member  = members(:one)
-    @period  = Period.create(project_id: @project.id, stage_id: @stage.id, member_id: @member.id)
-  end
 
   test "should show report for member" do
+    @project = projects(:one)
+    @stage = stages(:one)
+    @member = members(:one)
+    @period  = Period.create(project_id: @project.id, stage_id: @stage.id, member_id: @member.id)
+    @other_member_period = Period.create(project_id: 1, stage_id: 1, member_id: 999)
+
     get member_reports_url(member_id: @member), as: :json
     assert_response :success
     assert_not_empty assigns(:periods)
+    assert_empty assigns(:periods).select{|period| period.member_id != @member.id }
   end
 end
